299eb686ea9022948a10bd0762ba80edfbfa1368
Vasi-18 <vasanthkumar1822000@gmail.com>
2023-06-12 18:45:04 +0530

    Call Back Hell

--- call_back_hell/script.js
+++ call_back_hell/script.js
@@ -1,26 +1,43 @@
-const greet = (message = "", cb = (d) => {}) => {
-    console.log(message);
-    cb(message);
-  };
-  
-  greet(10, () => {
-    greet(9, () => {
-      greet(8, () => {
-        greet(7, () => {
-          greet(6, () => {
-            greet(5, () => {
-              greet(4, () => {
-                greet(3, () => {
-                  greet(2, () => {
-                    greet(1, () => {
-                      greet("Happy Independence Day!!!");
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
-    });
-  });
+window.onload = function() {
+
+    var txt = document.getElementById('text');
+    var msg = document.getElementById('msg');
+    msg.style.visibility = 'hidden';
+
+    txt.innerHTML = 10;
+    setTimeout(function() {
+        txt.innerHTML = 9;
+        setTimeout(function() {
+            txt.innerHTML = 8;
+            setTimeout(function() {
+                txt.innerHTML = 7;
+                setTimeout(function() {
+                    txt.innerHTML = 6;
+                    setTimeout(function() {
+                        txt.innerHTML = 5;
+                        setTimeout(function() {
+                            txt.innerHTML = 4;
+                            setTimeout(function() {
+                                txt.innerHTML = 3;
+                                setTimeout(function() {
+                                    txt.innerHTML = 2;
+                                    setTimeout(function() {
+                                        txt.innerHTML = 1;
+                                        setTimeout(function() {
+                                            msg.style.visibility = 'visible';
+                                            txt.style.visibility = 'hidden';
+                                            // setTimeout(function() {
+                                            //     msg.style.visibility = 'hidden';
+                                            // }, 3000)
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+
+}
